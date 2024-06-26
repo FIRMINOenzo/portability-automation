0f9c251f9a6376c0b70b008d7c2259e3628f91a5
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-06-26 19:41:12 -0300

    feat: add brb login step

--- src/steps/BRB/LoginBrb.step.ts
+++ src/steps/BRB/LoginBrb.step.ts
@@ -1,8 +1,22 @@
+import inquirer from 'inquirer';
+import { AlcifUrls } from '../../constants/Alcif';
 import { BotData } from '../../interfaces/BotData';
 import { Step } from '../../step';
 
 export class LoginBrbStep extends Step {
   async execute(botData: BotData): Promise<BotData> {
+    await this.bot.page.goto(`${AlcifUrls.BASE}/login`, {
+      waitUntil: 'networkidle2',
+    });
+    await this.bot.page.type('#email', botData.brbUser.email);
+    await this.bot.page.type('#password', botData.brbUser.password);
+
+    await inquirer.prompt({
+      type: 'confirm',
+      name: 'performCaptchaAndLogin',
+      message: 'Pressione enter após realizar o captcha e logar',
+    });
+
     return botData;
   }
 }
