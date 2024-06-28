3cda78c520dd91e96221f73c2cef77e86e347407
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-06-28 15:30:40 -0300

    refact: split function to improve single resp

--- src/services/consitech.service.ts
+++ src/services/consitech.service.ts
@@ -35,6 +35,84 @@ export class ConsitechService {
     return new ConsitechUser(user.email, user.password, response.data.token);
   }
 
+  // async getProposalData(
+  //   proposal: Proposal,
+  //   token: string
+  // ): Promise<BrbProposalData> {
+  //   try {
+  //     const response = await axios.post(
+  //       `${ConsitechUrls.BASE}/propostas/getOne`,
+  //       proposal,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+
+  //     const data = await response.data;
+  //     const convertedState = await this.convertState(data.uf);
+  //     const convertedAccount = await this.convertAccount(data.info_tipo_conta);
+  //     const convertedRegistration = await this.convertRegistration(
+  //       data.info_matricula
+  //     );
+  //     const convertedCity = await this.convertCity(
+  //       {
+  //         stateId: data.uf,
+  //         cityId: data.cidade,
+  //       },
+  //       token
+  //     );
+
+  //     if (!convertedState) throw new Error('Error converting state');
+  //     if (!convertedAccount) throw new Error('Error converting account');
+  //     if (!convertedCity) throw new Error('Error converting city');
+
+  //     const brbProposalData: BrbProposalData = {
+  //       simulation: {
+  //         cpf: data.cpf,
+  //         installmentValue: data.contrato_parcela,
+  //         outstandingBalance: data.contrato_saldo,
+  //         portedBank: data.contrato_banco,
+  //         portedContract: data.numeroDoContrato,
+  //         portedInstallment: data.contrato_parcela,
+  //         remainingInstallments: data.contrato_parcela_rest,
+  //         term: data.contrato_qtd_parcela,
+  //       },
+  //       personalData: {
+  //         birthDate: '',
+  //         birthCity: convertedCity,
+  //         birthState: convertedState,
+  //         maritalStatus: MaritalStatus.SINGLE,
+  //         name: data.nome,
+  //         motherName: data.docNomeDaMae,
+  //         registration: convertedRegistration,
+  //         sex: Sex.MALE,
+  //       },
+  //       address: {
+  //         addressType: AddressType.RESIDENTIAL,
+  //         complement: '',
+  //         neighborhood: data.bairro,
+  //         number: data.numero,
+  //         street: data.logradouro,
+  //         zipCode: data.cep,
+  //       },
+  //       contact: {
+  //         phone: data.telefone_celular,
+  //       },
+  //       bankData: {
+  //         account: data.info_conta,
+  //         agency: data.info_agencia,
+  //         bank: data.info_banco,
+  //         accountType: convertedAccount,
+  //       },
+  //     };
+  //     return brbProposalData;
+  //   } catch (error) {
+  //     throw new Error('Error getting proposal data');
+  //   }
+  // }
+
   async getProposalData(
     proposal: Proposal,
     token: string
@@ -50,66 +128,76 @@ export class ConsitechService {
         }
       );
 
-      const data = await response.data;
-      const convertedState = await this.convertState(data.uf);
-      const convertedAccount = await this.convertAccount(data.info_tipo_conta);
-      const convertedCity = await this.convertCity(
-        {
-          stateId: data.uf,
-          cityId: data.cidade,
-        },
-        token
-      );
+      if (!response.data) {
+        throw new Error('No data received from Consitech');
+      }
 
-      if (!convertedState) throw new Error('Error converting state');
-      if (!convertedAccount) throw new Error('Error converting account');
-      if (!convertedCity) throw new Error('Error converting city');
-
-      const brbProposalData: BrbProposalData = {
-        simulation: {
-          cpf: data.cpf,
-          installmentValue: data.contrato_parcela,
-          outstandingBalance: data.contrato_saldo,
-          portedBank: data.contrato_banco,
-          portedContract: data.numeroDoContrato,
-          // valor da parcela!!!!
-          portedInstallment: data.contrato_parcela,
-          remainingInstallments: data.contrato_parcela_rest,
-          term: data.contrato_qtd_parcela,
-        },
-        personalData: {
-          birthDate: '',
-          birthCity: convertedCity,
-          birthState: convertedState,
-          maritalStatus: MaritalStatus.SINGLE,
-          name: data.nome,
-          motherName: data.docNomeDaMae,
-          // matricula "beneficio": "1738771498 - RJ", SOMENTE O NUMERO
-          registration: data.beneficio,
-          sex: Sex.MALE,
-        },
-        address: {
-          addressType: AddressType.RESIDENTIAL,
-          complement: '',
-          neighborhood: data.bairro,
-          number: data.numero,
-          street: data.logradouro,
-          zipCode: data.cep,
-        },
-        contact: {
-          phone: data.telefone_celular,
-        },
-        bankData: {
-          account: data.info_conta,
-          agency: data.info_agencia,
-          bank: data.info_banco,
-          accountType: convertedAccount,
-        },
-      };
-      return brbProposalData;
+      return this.convertDataToBrbProposalData(response.data, token);
     } catch (error) {
-      throw new Error('Error getting proposal data');
+      console.error('Error getting proposal data:', error);
+      throw error;
     }
+  }
+
+  private async convertDataToBrbProposalData(
+    data: any,
+    token: string
+  ): Promise<BrbProposalData> {
+    const convertedState = await this.convertState(data.uf);
+    const convertedAccount = await this.convertAccount(data.info_tipo_conta);
+    const convertedRegistration = await this.convertRegistration(
+      data.info_matricula
+    );
+    const convertedCity = await this.convertCity(
+      { stateId: data.uf, cityId: data.cidade },
+      token
+    );
+
+    if (!convertedState) throw new Error('Error converting state');
+    if (!convertedAccount) throw new Error('Error converting account');
+    if (!convertedCity) throw new Error('Error converting city');
+
+    const brbProposalData: BrbProposalData = {
+      simulation: {
+        cpf: data.cpf,
+        installmentValue: data.contrato_parcela,
+        outstandingBalance: data.contrato_saldo,
+        portedBank: data.contrato_banco,
+        portedContract: data.numeroDoContrato,
+        portedInstallment: data.contrato_parcela,
+        remainingInstallments: data.contrato_parcela_rest,
+        term: data.contrato_qtd_parcela,
+      },
+      personalData: {
+        birthDate: '',
+        birthCity: convertedCity,
+        birthState: convertedState,
+        maritalStatus: MaritalStatus.SINGLE,
+        name: data.nome,
+        motherName: data.docNomeDaMae,
+        registration: convertedRegistration,
+        sex: Sex.MALE,
+      },
+      address: {
+        addressType: AddressType.RESIDENTIAL,
+        complement: '',
+        neighborhood: data.bairro,
+        number: data.numero,
+        street: data.logradouro,
+        zipCode: data.cep,
+      },
+      contact: {
+        phone: data.telefone_celular,
+      },
+      bankData: {
+        account: data.info_conta,
+        agency: data.info_agencia,
+        bank: data.info_banco,
+        accountType: convertedAccount,
+      },
+    };
+
+    return brbProposalData;
   }
 
   private async convertState(stateId: number): Promise<State | null> {
@@ -143,5 +231,9 @@ export class ConsitechService {
     account: string
   ): Promise<BankAccountType | null> {
     return getBankAccountTypeFromAcronym(account);
+  }
+
+  private async convertRegistration(registration: string): Promise<string> {
+    return registration.split(' - ')[0];
   }
 }
