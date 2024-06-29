e64a0eececd5369e806aca33c20d9e72e773339c
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-06-29 18:30:51 -0300

    refact: load user data from json file

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,18 +1,29 @@
-import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const getEnv = () => ({
-  user: {
-    brb: {
-      email: process.env.ACCOUNT_EMAIL,
-      password: process.env.ACCOUNT_PASSWORD,
-    },
-    consitech: {
-      email: process.env.CONSITECH_EMAIL,
-      password: process.env.CONSITECH_PASSWORD,
-    },
-  },
-});
+const getConfig = () => {
+  const configPath = path.join(__dirname, '../../config.json');
+  if (!fs.existsSync(configPath)) {
+    throw new Error(
+      'Arquivo de configuração não encontrado. Por favor, crie um arquivo config.json na raiz do projeto.'
+    );
+  }
 
-dotenv.config();
+  const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-export const env = getEnv();
+  if (!config.user?.brb?.email || !config.user?.brb?.password) {
+    throw new Error(
+      'Dados de login BRB não fornecidos no arquivo de configuração.'
+    );
+  }
+
+  if (!config.user?.consitech?.email || !config.user?.consitech?.password) {
+    throw new Error(
+      'Dados de login Consitech não fornecidos no arquivo de configuração.'
+    );
+  }
+
+  return config;
+};
+
+export const env = getConfig();
